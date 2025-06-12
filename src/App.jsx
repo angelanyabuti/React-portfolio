4dd41a19c49ac7cb3d27948c6e21cbbbfb9430e8
Angela <nyabutiangella@gmail.com>
2025-06-12 13:16:37 +0300

    Created the offers section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Products from './components/Products';
 import Categories from './components/Categories';
+import Offers from './components/Offers';
 
 const App = () => {
   return(
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <Products/>
       <Categories/>
+      <Offers/>
     </div>
   )
 }
